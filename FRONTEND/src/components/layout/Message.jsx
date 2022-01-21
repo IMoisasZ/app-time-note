4ebdc7854c9917fbc3a7002989031a97ed9e7a49
Moisas <mopri08@gmail.com>
2022-01-21 18:05:25 -0300

    continues to make a messages of system

--- FRONTEND/src/components/layout/Message.jsx
+++ FRONTEND/src/components/layout/Message.jsx
@@ -1,27 +1,45 @@
 function Message({ message, type }) {
 	return (
-		<div>
-			<p
-				style={
-					type === 'error'
-						? {
-								backgroundColor: '#rgba(221, 92, 92, .5)',
-								padding: '0.5em',
-								borderRadius: '0.5em',
-								marginTop: '1em',
-						  }
-						: {
-								backgroundColor: 'rgba(69, 177, 69, .5)',
-								padding: '0.5em',
-								borderRadius: '0.5em',
-								marginTop: '1em',
-						  }
-				}
-			>
-				{message}
-			</p>
-		</div>
+		<>
+			{type && (
+				<div
+					style={
+						type === 'error'
+							? {
+									backgroundColor: 'rgba(221, 92, 92, .5)',
+									padding: '0.5em',
+									borderRadius: '0.5em',
+									marginTop: '1em',
+							  }
+							: {
+									backgroundColor: 'rgba(69, 177, 69, .5)',
+									padding: '0.5em',
+									borderRadius: '0.5em',
+									marginTop: '1em',
+							  }
+					}
+				>
+					{message}
+				</div>
+			)}
+		</>
 	)
 }
 
 export default Message
+
+// style={
+// 	type === 'error'
+// 		? {
+// 				backgroundColor: '#rgba(221, 92, 92, .5)',
+// 				padding: '0.5em',
+// 				borderRadius: '0.5em',
+// 				marginTop: '1em',
+// 		  }
+// 		: {
+// 				backgroundColor: 'rgba(69, 177, 69, .5)',
+// 				padding: '0.5em',
+// 				borderRadius: '0.5em',
+// 				marginTop: '1em',
+// 		  }
+// }
